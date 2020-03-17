afe080be09d55471643721a03f30a9f306fc5992
kadd0ury <kaddoury2017@gmail.com>
2020-03-17 13:48:10 +0000

    launching

---
+++ blog-cms/app.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var passport = require('passport');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+
+
+
+
+
+
+
+app.use(passport.initialize());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+mongoose.connect('mongodb://localhost/blog-cms', {
+ promiseLibrary: require('bluebird'),
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useCreateIndex: true
+}).then(() => console.log('connection successful'))
+ .catch((err) => console.error(err));
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var auth = require('./routes/auth');
+var category = require('./routes/category');
+var post = require('./routes/post');
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use('/api/auth', auth);
+app.use('/api/category', category);
+app.use('/api/post', post);
+
+
+
+
+module.exports = app;
